450d4103372af094797007a90e539dfa1f5e8e49
adamrajch <arajchwald@gmail.com>
2021-09-16 16:45:06 -0500

    added styles and components and auth functionality

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,18 +1,12 @@
-import { useRouter } from "next/router";
-import React, { ReactElement, useEffect } from "react";
-import { useAuthState } from "../../context/userContext";
+import React, { ReactElement } from "react";
+import Dashboard from "../../components/shell/dashboard";
 
 export default function DashHome(): ReactElement {
-  const { isAuthenticated, user } = useAuthState();
-  const router = useRouter();
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.push("login");
-    }
-  }, [isAuthenticated]);
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
-  return <div>dash home</div>;
+  return (
+    <div>
+      <Dashboard>
+        <div>Dashboard </div>
+      </Dashboard>
+    </div>
+  );
 }
